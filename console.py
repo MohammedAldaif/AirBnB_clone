8266bbbb8494a173bfbcfd5dbe1dc72198a0caf8
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-02-07 23:49:01 -0800

    m

---
+++ console.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import json
+import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+'''
+entry point for the interpreter
+'''
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
+    def do_EOF(self,line):
+        ''' quit the interprete when the user presses ctrl + d '''
+        return True
+    def do_quit(self,line):
+        ''' quit the interpreter when the user types quit '''
+        return True
+    def emptyline(self):
+        ''' override the origianal method to make it return nothing
+        when the user press enter and the input line is empty '''
+        pass
+    def do_create(self,line):
+        classes_names = globals()
+        if line in classes_names:
+            obj = classes_names[line]()
+            obj_dictionary = obj.to_dict() 
+            file_storage_object = FileStorage()
+            file_storage_object.new(obj)
+            file_storage_object.save()
+            # Print the 'id' from the saved JSON file
+            with open(FileStorage._FileStorage__file_path, 'r') as json_file:
+                loaded_dict = json.load(json_file)
+                key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                if key in loaded_dict:
+                    print(loaded_dict[key]['id'])
+        elif not line:
+            print("** class name missing **")
+        elif line not in classes_names:
+            print("** class doesn't exist **")
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,55 @@
 #!/usr/bin/python3
 """
-Module for BaseModel class
+Module for the BaseModel class
 """
+
 import uuid
 from datetime import datetime
 
 class BaseModel:
     """
-    the BaseModel class defines common attributes/methods for other classes
+    define the attributes and methods for all other classes
     """
     def __init__(self, *args, **kwargs):
         """
-        create a new instance of BaseModel class
+        this method got executed each time an object of this class is created
         """
+        from models import storage
+
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         """
-        this method returns a string representation of the BaseModel instance
+        this method returns a string representation of BaseModel
         """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
-        this method pdates the attribute updated_at with the current datetime
+    this method updates the attribute updated_at with the current datetime
         """
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
-        this method eturns a dictionary representation of the BaseModel instance
+        this method returns a dictionary containing all keys/values of __dict__ of the instance
         """
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
