e8e19219afc09c20ae782152bed622588ffecfcd
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2024-02-09 07:42:05 -0800

    a

---
+++ models/amenity.py
@@ -0,0 +1,2 @@
+ def init(self, name):
+        self.name = str(name)
---
+++ models/review.py
@@ -0,0 +1,4 @@
+def init(self, place_id, user_id, text):
+    self.place_id = str(place_id)
+        self.user_id = str(user_id)
+        self.text = str(text)
