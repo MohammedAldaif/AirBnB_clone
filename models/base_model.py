dca5fc3655699704382ba60443dbfa8716ff0082
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2024-02-09 02:43:17 -0800

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,9 @@ class BaseModel:
         """
         create a new instance of BaseModel class
         """
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
